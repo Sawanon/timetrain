2e186d395a90f4621f3cad6f178d502535a2fd53
Sawanon <sawanon01@hotmail.com>
2019-05-08 08:57:20 +0700

    perfect #08/05/2019

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <style type="text/css">
     table{
+      color: #ffffff;
       text-align: center;
     }
       td{
@@ -59,6 +60,7 @@
     echo "<br>check ";
     print_r($check);
     $dayma = 0;
+    $workedday = 0;
     //$hrtotal = 0;
     for($j=0;$j<3;$j++){
       $dayt = 0;
@@ -67,8 +69,8 @@
       $count = 0;
       $dayshow = 1;
       echo "<table border='1'>";
-      echo "<tr><th colspan='7'>".$pm[$j]."</th></tr>";
-      echo "<tr><th style='background-color: #dee464;'>จันทร์</th><th style='background-color: #f06de3;'>อังคาร</th><th style='background-color: #7dd065;'>พุธ</th>
+      echo "<tr><th colspan='7' style='background-color: #1a5e86;'>".$pm[$j]."</th></tr>";
+      echo "<tr><th style='background-color: #cfc700;'>จันทร์</th><th style='background-color: #f06de3;'>อังคาร</th><th style='background-color: #7dd065;'>พุธ</th>
       <th style='background-color: #dd8f3f;'>พฤหัสบดี</th><th style='background-color: #3498d0;'>ศุกร์</th><th style='background-color: #a147e8;'>เสาร์</th>
       <th style='background-color: #e74c4c;'>อาทิตย์</th></tr>";
       for($i=0;$i<$maxday;$i++) {
@@ -82,14 +84,15 @@
           if($startday<=$dayt){
             if($day[$pm[$j]][$dayma]==$dayshow){
               if(substr($day[$pm[$j]][$dayma+2],1,2)==1){
-                echo "<td style='background-color: #a4eb5e;'>".$dayshow."</td>";
+                echo "<td style='background-color: #569616;'>".$dayshow."</td>";
                 $hrtotal += 9;
+                $workedday += 1;
               }else if(substr($day[$pm[$j]][$dayma+2],1,2)==2){
-                echo "<td style='background-color: #eee965;'>".$dayshow."</td>";
+                echo "<td style='background-color: #c2b93d;'>".$dayshow."</td>";
               }
               $dayma = $dayma+4;
             }else{
-              echo "<td style='background-color: #df6868;'>".$dayshow."</td>";
+              echo "<td style='background-color: #8c1313;'>".$dayshow."</td>";
             }
             $dayshow++;
           }else{
@@ -103,7 +106,16 @@
       }
       echo "</table><br>";
     }
-    echo "<div style='position: fixed; top: 50%; right: 30%; color: #f2eae9;'>total: ".$hrtotal."</div>";
+    $left = 240-$hrtotal;
+    $leftday = $left/9;
+    $leftdaywork = 32-$workedday;
+    echo "<div style='position: fixed; top: 50%; right: 30%; color: #d7bcb8;'>
+    total : ".$hrtotal." Hr <br>
+    Left : ".$left." Hr <br>
+    Left Days : ".substr($leftday,0,5)." Days <br><br>
+    Worked Days : ".$workedday." Days <br>
+    Left Work Days : ".$leftdaywork." Days
+    </div>";
     ?>
     <script src="bootstrap/js/bootstrap.min.js"></script>
   </body>
